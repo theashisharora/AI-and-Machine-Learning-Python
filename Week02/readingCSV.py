012cb5b49ede5029a6f50a9235fe0a8fe5bd86d0
Ash <167486209+theashisharora@users.noreply.github.com>
2024-05-23 17:30:14 -0400

    commit

--- Week02/readingCSV.py
+++ Week02/readingCSV.py
@@ -1,4 +1,27 @@
 import numpy as np
 import pandas as pd
 
-titanicf = pd.read_csv('Titanic-Dataset.csv')
+titanicf = pd.read_csv(r'C:\Users\ashis\AI-and-Machine-Learning-Python-1\Week02\Titanic-Dataset.csv')
+
+
+
+titanicf.drop('Cabin',axis=1,inplace=True)
+titanicf.drop('Embarked', axis=1, inplace=True)
+titanicf.drop('PassengerId', axis=1, inplace=True)
+
+print(titanicf.info())
+
+print(titanicf.head(5))
+
+print(titanicf[titanicf['Age'].isnull()])
+
+indexAge = titanicf[titanicf['Age'].isnull()].index
+titanicf.drop(indexAge, inplace=True)
+
+print(titanicf[titanicf['Age'].isnull()])
+print(titanicf.info())
+
+gender = titanicf.replace('male',0,inplace=True)
+print(titanicf.info())
+
+print(titanicf.head(5))
